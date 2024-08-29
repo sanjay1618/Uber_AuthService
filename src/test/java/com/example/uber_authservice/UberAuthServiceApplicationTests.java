54b1a5ef8ef3968a6591618ee2c90c24473b94db
Sanjay <sanjay.uppugandla@gmail.com>
2024-08-29 11:39:10 -0700

    First commit with inital set up

---
+++ src/test/java/com/example/uber_authservice/UberAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uber_authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
