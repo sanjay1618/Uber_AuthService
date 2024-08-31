c2dc5f89c5b78927e7ffd721af7ee7f72f660acd
Sanjay <sanjay.uppugandla@gmail.com>
2024-08-31 00:26:12 -0700

    Added jwt extraction and validation methods

--- src/main/java/com/example/uber_authservice/services/JwtService.java
+++ src/main/java/com/example/uber_authservice/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.example.uber_authservice.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,18 +22,45 @@ public class JwtService implements CommandLineRunner{
     @Value("${jwt.secret}")
     private String secret;
 
-    public String createToken(Map<String, Object> claims, String username) {
+    public String createToken(Map<String, Object> claims, String email) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + expiry*1000L);
-        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
         return Jwts.builder()
                 .claims(claims)
                 .expiration(expiryDate)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .signWith(key)
+                .signWith(getSecretKey())
+                .subject(email)
                 .compact();
-
     }
+
+    public Key getSecretKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Claims getClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private String extractUsernameFromToken(String token) {
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return getClaimsFromToken(token).getExpiration().before(new Date());
+    }
+
+    private Boolean validateToken(String token, String email) {
+        final String userEmail = getClaimsFromToken(token).getSubject();
+        return userEmail.equals(email) && isTokenExpired(token);
+    }
+
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -42,6 +71,5 @@ public class JwtService implements CommandLineRunner{
         System.out.println(token);
 
     }
-
 
 }
